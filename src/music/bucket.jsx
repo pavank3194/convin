753123cb6ef52382f09cc9a91ac3eb777da8ab87
Pavan Kalyan <108045346+pavank3194@users.noreply.github.com>
2023-05-23 15:32:43 +0530

    Update bucket.jsx

--- src/music/bucket.jsx
+++ src/music/bucket.jsx
@@ -7,7 +7,7 @@ function Bucket() {
     <div>
       <div className="bucket-names">
         <div className="bucket1">
-          <a href="/entSongs"><b>Entertainment Songs</b></a>
+          <a href="/entSongs"><b>Entertainment Songs1</b></a>
         </div>
         <div className="bucket2">
         <a href="/entVideos"><b>Entertainment videos</b></a>
